fcfb5a417dfa5d32feee07c73c2dd58c256a0f45
Dinesh-R <dinesh@sparkouttech.com>
2024-06-12 11:30:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    columns: {
+      '3xs':'10rem'
+    }
+  },
+  plugins: [],
+}
+
